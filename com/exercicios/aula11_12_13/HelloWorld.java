9f65e605235b7df60f1a02fd3be2a8f91bc607d9
Glenelly <glenellykarlatbm@gmail.com>
2023-05-09 17:31:39 -0300

    First commit Hello World

---
+++ com/exercicios/aula11_12_13/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.exercicios.aula11_12_13;
+
+public class HelloWorld {
+
+    //Faça um Programa que mostre a mensagem "Alo mundo" na tela
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!!");
+    }
+}
