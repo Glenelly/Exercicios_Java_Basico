e27afb77e3ddb2846fecf3a6aab394d6b5c4ca91
Glenelly <glenellykarlatbm@gmail.com>
2023-05-21 18:13:42 -0300

    Exercício - Série de Fibonacci maior que 500

---
+++ com/exercicios/aula16_17/Fibonacci2.java
@@ -0,0 +1,24 @@
+package com.exercicios.aula16_17;
+
+public class Fibonacci2 {
+    public static void main(String[] args) {
+
+        /*
+        A série de Fibonacci é formada pela seqüência 0,1,1,2,3,5,8,13,21,34,55,...
+        Faça um programa que gere a série até que o valor seja maior que 500.
+        */
+
+        int primeiro = 1;
+        int segundo = 1;
+        int proximo = 0;
+
+            while (proximo <= 500){
+
+                proximo = primeiro + segundo;
+                primeiro = segundo;
+                segundo = proximo;
+
+                System.out.println(proximo);
+        }
+    }
+}
