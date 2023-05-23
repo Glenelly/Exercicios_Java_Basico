6ae4fb006dc5686f99b5bdd4c3d7c78a752afb81
Glenelly <glenellykarlatbm@gmail.com>
2023-05-23 10:24:29 -0300

    Comentando exercício Media alunos

--- com/exercicios/aula16_17/MediaAlunos.java
+++ com/exercicios/aula16_17/MediaAlunos.java
@@ -20,32 +20,43 @@ public class MediaAlunos {
 
         Scanner scan = new Scanner(System.in);
 
+        //Variaveis
         int turma;
         int aluno;
         int media;
         int soma = 0;
         boolean invalido;
 
+        //Pedindo a quantidade de turmas
         System.out.println("Informe a quantidade de turmas: ");
         turma = scan.nextInt();
 
+        ////acrescentar enquanto o i for menor que turma
         for(int i = 1; i <= turma; i++){
 
+            //Parar o loop quando a variavel invalido = false
             invalido = true;
             do{
+                //informar a quantidade de alunos
                 System.out.println("Informe a quantidade de alunos desta turma " + i + ":");
                 aluno = scan.nextInt();
 
+                //verificando se a turma tem mais de 40 alunos
                 if (aluno <= 40){
                     invalido = false;
+                //caso tenha mais de 40 alunos exibe a mensagem e executa o loop (invalido  = true)
                 }else {
                     System.out.println("A turma só pode ter até 40 alunos! Digite novamente: ");
                 }
             }while (invalido);
-            
+
+            //Soma os alunos de todas as turmas
             soma += aluno;
         }
+
+        //Soma a média de alunos por turma
         media = soma / turma;
+        //Exibe o resultado
         System.out.println("A média de alunos por turma é de: " + media);
     }
 }
