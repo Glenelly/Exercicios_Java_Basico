bd6353e56729bd1f01d69c961d711ead1d7ecaac
Glenelly <glenellykarlatbm@gmail.com>
2023-05-31 00:06:27 -0300

    Exercício - Números primos

---
+++ com/exercicios/aula16_17/NumPrimo.java
@@ -0,0 +1,16 @@
+package com.exercicios.aula16_17;
+
+import java.util.Scanner;
+
+public class NumPrimo {
+    public static void main(String[] args) {
+        /*
+        Faça um programa que peça um número inteiro e determine se ele é ou não um número primo.
+        Um número primo é aquele sendo divisível somente por ele mesmo e por 1
+        */
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe um número inteiro: ");
+    }
+}
